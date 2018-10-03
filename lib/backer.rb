09e38ab8486c086ff11474b9f24edad5eff0489f
Zachary Lehmann <zlehmann@gmail.com>
2018-10-03 01:18:37 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,13 @@
 class Backer
-  attr_accessor :name
+  attr_accessor :name, :backed_projects
+
 
   def initialize(name)
     @name = name
+    @backed_project = []
   end
 
+  def back_project(project)
+    @backed_projects << project
+  end
 end
