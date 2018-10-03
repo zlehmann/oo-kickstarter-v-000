c64f796f9d2db4d2c9c582d9e4bb207b12d24e26
Zachary Lehmann <zlehmann@gmail.com>
2018-10-03 01:16:29 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,8 @@
 class Backer
-  attr_accessor :name 
-  
+  attr_accessor :name
+
   def initialize(name)
     @name = name
   end
-  
+
 end
---
+++ lib/project.rb
@@ -0,0 +1,9 @@
+class Project
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+  end
+
+
+end
